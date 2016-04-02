9bb4652e1765ffd336fb9e92f76e674ae623f203
skwang <skwang@princeton.edu>
2016-04-02 15:51:23 -0400

    front-end mockup done

--- atw_app/views.py
+++ atw_app/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from clarifai.client import ClarifaiApi
-import urllib
+from urlparse import urlparse
+import httplib
+
 # from models import *
 # from forms import *
 # import random
@@ -10,10 +12,34 @@ import urllib
 def index(request):
     return render(request, 'atw_app/index.html')
 
-
 def generate_quote(request):
     url = request.GET['url']
-    image = urllib.URLopener()
-    quote = 'A picture is worth a thousand words'
-    speaker = 'Anonymous'
-    return JsonResponse({'quote':quote, 'speaker':speaker})
+    if not verify_image(url):
+        quote = 'The fattest chicken is the first to go'
+        speaker = 'They'
+        valid = False
+        return JsonResponse({'quote':quote, 'speaker':speaker, 'valid':valid})
+    else:
+        quote = 'The fattest chicken is the first to go'
+        speaker = 'They'
+        valid = True
+        return JsonResponse({'quote':quote, 'speaker':speaker, 'valid':valid})
+
+# Helper method to verify if a given string url points to an image
+def verify_image(url):
+    # requires the http
+    if "http" not in url:
+        url = "http://" + url
+    parse_object = urlparse(url)
+    try:
+        conn = httplib.HTTPConnection(parse_object.netloc)
+        conn.request("HEAD", parse_object.path)
+        res = conn.getresponse()
+        headers = res.getheaders()
+        for header in headers:
+            if header[0] == 'content-type':
+                if 'image' in header[1]:
+                    return True
+        return False
+    except:
+        return False
