ddb27655c7b23a8be2db671c7c712a576b3bb5d5
skwang <skwang@princeton.edu>
2016-04-02 21:33:21 -0400

    merging

--- atw_app/views.py
+++ atw_app/views.py
@@ -4,30 +4,36 @@ from clarifai.client import ClarifaiApi
 from urlparse import urlparse
 import httplib
 
-# from models import *
-# from forms import *
-# import random
-
 
 def index(request):
     return render(request, 'atw_app/index.html')
 
 def generate_quote(request):
     url = request.GET['url']
-    if not verify_image(url):
+    if not verify_image(url): # not a valid image url
         quote = 'The fattest chicken is the first to go'
         speaker = 'They'
         valid = False
         return JsonResponse({'quote':quote, 'speaker':speaker, 'valid':valid})
-    else:
+    else: 
+        # tags, probs = get_tags_and_probs(url)
         quote = 'The fattest chicken is the first to go'
         speaker = 'They'
         valid = True
         return JsonResponse({'quote':quote, 'speaker':speaker, 'valid':valid})
 
+def get_tags_and_probs(url):
+    clarifai_api = ClarifaiApi()  
+    result = clarifai_api.tag_image_urls(url)
+    tags = result['results'][0]['result']['tag']['classes']
+    probs = result['results'][0]['result']['tag']['probs']
+    print tags
+    print probs
+    return tags, probs
+
 # Helper method to verify if a given string url points to an image
 def verify_image(url):
-    # requires the http
+    # requires the http in the start of the url to work
     if "http" not in url:
         url = "http://" + url
     parse_object = urlparse(url)
